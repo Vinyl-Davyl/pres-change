33241789f3c8138f3bc0e06a14824eb6c91940b9
Vinyl-Davyl <okononfuadavid@gmail.com>
2025-05-23 17:58:00 +0100

    Development

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,32 @@
 // import Image from 'next/image'
 // import wallet from '../assets/icons/wallet.svg'
-import React from 'react'
-import { ConnectButton } from 'thirdweb/react'
-import { client } from '../utils/client'
+import React from "react";
+import { ConnectButton } from "thirdweb/react";
+import { client } from "../utils/client";
 
-import { inAppWallet, createWallet } from 'thirdweb/wallets'
+import { inAppWallet, createWallet } from "thirdweb/wallets";
 
 const wallets = [
   inAppWallet({
     auth: {
-      options: [
-        'google',
-        'discord',
-        'farcaster',
-        'email',
-        'phone',
-        'coinbase',
-        'passkey',
-      ],
+      options: ["google", "discord", "farcaster", "email", "phone", "coinbase", "passkey"],
     },
   }),
-  createWallet('io.metamask'),
-  createWallet('com.coinbase.wallet'),
-  createWallet('me.rainbow'),
-  createWallet('io.rabby'),
-  createWallet('io.zerion.wallet'),
-]
+  // createWallet("sui.wallet"),
+  // createWallet("com.suiet"),
+  // createWallet("io.ethoswallet"),
+  // createWallet("com.martianwallet"),
+  // createWallet("io.suipay"),
+];
 
 const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center p-4 text-white">
-      <h2 className="text-2xl font-bold -ml-4">Classroom</h2>
+      <h2 className="text-2xl font-bold -ml-4">Sui Classroom</h2>
       <div className="flex-1 flex justify-center">
         <input
           type="text"
-          placeholder="Search Topics, project or Narrative"
+          placeholder="Search Sui Topics, Move Modules or Projects"
           className="w-96 h-11 px-5 py-3  bg-white/5 rounded-3xl justify-start items-center gap-2 inline-flex focus:outline-none focus:ring-2 focus:ring-purple-600"
         />
       </div>
@@ -47,15 +39,11 @@ const Header: React.FC = () => {
           className="mr-2"
         /> */}
       <div className="ml-8">
-        <ConnectButton
-          client={client}
-          wallets={wallets}
-          connectModal={{ size: 'wide' }}
-        />
+        <ConnectButton client={client} wallets={wallets} connectModal={{ size: "wide" }} />
       </div>
       {/* </button> */}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
